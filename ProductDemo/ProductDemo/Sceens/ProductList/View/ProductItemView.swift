6edf060d3d64c5c99e270a5da26e9af391d0bff3
Md Shafiq Ullah <shohagbit0404@gmail.com>
2024-11-20 23:51:55 -0800

    refactor SwiftUI

--- ProductDemo/ProductDemo/Sceens/ProductList/View/ProductItemView.swift
+++ ProductDemo/ProductDemo/Sceens/ProductList/View/ProductItemView.swift
@@ -11,34 +11,16 @@ struct ProductItemView: View {
     
     var product: Product
     
+    
+    
+    
+    
     var body: some View {
         HStack{
             // image
             //            Image(systemName: "globe")
             //                .frame(width: 100, height: 100)
-            AsyncImage(url: URL(string: product.image)) { result in
-                
-                switch result {
-                case .empty:
-                    ProgressView()
-                        .frame(width: 100, height: 100)
-                case .success(let image):
-                    image.resizable()
-                        .frame(width: 100, height: 100)
-                        .clipped()
-                case .failure(let error):
-                    Image(systemName: "xmark.circle")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 100, height: 100)
-                        .foregroundColor(.red)
-                    
-                @unknown default:
-                    EmptyView()
-                }
-                
-            }
-            
+            imageGenerateView(product: product)
             
             VStack(alignment: .leading){
                 //Name
@@ -47,45 +29,41 @@ struct ProductItemView: View {
                     .bold()
                 
                 HStack{
-                    //Category
-                    Text(product.category)
-                        .font(.subheadline)
-                        .foregroundStyle(.gray)
+                    grayTextView(product.category)
                     
                     Spacer()
                     
                     //rating
-                    HStack{
-                        Image(systemName: "star")
-                        
-                        Text(String(format: "%.1f", product.rating.rate))
-                    }.foregroundStyle(.orange)
+                    ratingView(product: product)
                 }
                 
                 //description
-                Text(product.description)
-                    .font(.subheadline)
-                    .foregroundStyle(.gray)
+                grayTextView(product.description)
                 
                 HStack{
-                    //Price
-                    Text("$"+String(format: "%.2f", product.price))
-                        .font(.title2)
-                        .foregroundStyle(.blue)
-                    
+                    productPrice()
                     
                     Spacer()
                     
                     //buy button
-                    Button {
-                        // Action
-                    } label: {
-                        Text("Buy")
-                    }.padding()
-                    
+                    buyButtonView()
                 }
             }
         }.padding()
+    }
+    
+    fileprivate func grayTextView(_ text: String) -> Text {
+        return //Category
+        Text(text)
+            .font(.subheadline)
+            .foregroundStyle(.gray)
+    }
+    
+    fileprivate func productPrice() -> Text {
+        return //Price
+        Text("$"+String(format: "%.2f", product.price))
+            .font(.title2)
+            .foregroundStyle(.blue)
     }
 }
 
@@ -100,4 +78,55 @@ struct ProductItemView: View {
         rating: Rating(rate: 4.5, count: 230)
     )
     )
+}
+
+struct imageGenerateView: View {
+    var product: Product
+    var body: some View {
+        AsyncImage(url: URL(string: product.image)) { result in
+            
+            switch result {
+            case .empty:
+                ProgressView()
+                    .frame(width: 100, height: 100)
+            case .success(let image):
+                image.resizable()
+                    .frame(width: 100, height: 100)
+                    .clipped()
+            case .failure(let error):
+                Image(systemName: "xmark.circle")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 100, height: 100)
+                    .foregroundColor(.red)
+                
+            @unknown default:
+                EmptyView()
+            }
+            
+        }
+    }
+}
+
+
+struct ratingView: View {
+    var product: Product
+    
+    var body: some View {
+        HStack{
+            Image(systemName: "star")
+            
+            Text(String(format: "%.1f", product.rating.rate))
+        }.foregroundStyle(.orange)
+    }
+}
+
+struct buyButtonView: View {
+    var body: some View {
+        Button {
+            // Action
+        } label: {
+            Text("Buy")
+        }.padding()
+    }
 }
